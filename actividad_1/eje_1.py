ec075d0913c239e3c162a6464284be7c4ecde935
Miguel Angel Guerrero Rave <angel.guerreo999@gmail.com>
2025-08-15 11:37:17 -0500

    Add files via upload

---
+++ actividad_1/eje_1.py
@@ -0,0 +1,10 @@
+# ü .Crea una clase Racional que permita trabajar con números racionales (fracciones). Incluye los siguientes métodos: constructores (por defecto y parametrizado), accedentes, leer(), suma, resta, multiplicación, división.
+
+class racional:
+
+    def suma (b):
+        return b * 2
+    
+    def suma (a, b, c):
+        return a + b + c
+
